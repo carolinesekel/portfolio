a0528af41886f7d6d96b8616a141e7b21305a9d8
Caroline Sekel <carolinesekel@carolines-mbp-2.lan>
2020-08-20 15:03:31 -0400

    fixing css for testimonials

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
 jQuery(document).ready(function($) {
+    //PAGE TRANSITIONS
     $(".internal").on("click", function(event) {
-        //console.log('clicked')
+            //console.log('clicked')
 
-        event.preventDefault()
+            event.preventDefault()
 
-        const href = $(this).attr("href")
-            //console.log(href)
+            const href = $(this).attr("href")
+                //console.log(href)
 
-        window.history.pushState(null, null, href)
+            window.history.pushState(null, null, href)
 
-        $(".internal").removeClass("selected")
-            //if clicked name in upper left to go home also 
-        if (href == "index.html") {
-            $(".home").addClass("selected")
-        }
-        //if clicked a different internal link
-        else {
-            $(this).addClass("selected")
-        }
-
-
-        $.ajax({
-            url: href,
-            success: function(data) {
-                $(".main-content").fadeOut(250, function() {
-                    const newPage = $(data).filter(".main-content").html()
-
-                    $(".main-content").html(newPage)
-
-                    $(".main-content").fadeIn(250)
-                })
+            $(".internal").removeClass("selected")
+                //if clicked name in upper left to go home also 
+            if (href == "index.html") {
+                $(".home").addClass("selected")
             }
-        })
+            //if clicked a different internal link
+            //styling only changes for nav bar because css for selected only set for nav .selected
+            else {
+                $(this).addClass("selected")
+            }
 
-    })
+
+            $.ajax({
+                url: href,
+                success: function(data) {
+                    $(".main-content").fadeOut(250, function() {
+                        const newPage = $(data).filter(".main-content").html()
+
+                        $(".main-content").html(newPage)
+
+                        $(".main-content").fadeIn(250)
+                    })
+                }
+            })
+
+        })
+        //DARK MODE TOGGLE
+
 })
